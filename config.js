1342d09f02a36149d960ac4c050af62aba3805c1
Salvatore Capolupo <s.capolupo@gmail.com>
2025-05-13 09:33:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GIST_URL = "https://gist.githubusercontent.com/tuo-nome-utente/tuo-id-gist/raw/yourfile.json";
+const GIST_URL = "https://gist.githubusercontent.com/salvatorecapolupo/428c601a6270414a9d1d6e8da422a508/raw/50f7fc32db98d0a88a4b365be30cab41c0cf8645/thelarch.json";
